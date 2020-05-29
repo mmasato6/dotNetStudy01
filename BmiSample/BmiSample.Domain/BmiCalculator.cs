23cb27b29225c99b418aa1a97341416b9b6f410c
mutsuya masato <mutsuya_masato@yahoo.co.jp>
2020-05-29 16:36:20 +0900

    正常系実装

--- BmiSample/BmiSample.Domain/BmiCalculator.cs
+++ BmiSample/BmiSample.Domain/BmiCalculator.cs
@@ -4,5 +4,12 @@ namespace BmiSample.Domain
 {
     public class BmiCalculator
     {
+        public double Calculate(int heightCm,int weightKg)
+        {
+            const int CentimeterPerMeter = 100;
+            double heightM = heightCm / CentimeterPerMeter;
+            return (weightKg / (Math.Pow(heightM,2)));
+        }
+
     }
 }
--- BmiSample/BmiSample.DomainTest/BmiCalculatorTest.cs
+++ BmiSample/BmiSample.DomainTest/BmiCalculatorTest.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using BmiSample.Domain;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace BmiSample.DomainTest
@@ -6,8 +8,30 @@ namespace BmiSample.DomainTest
     public class BmiCalculatorTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void CalculatTest()
         {
+            // Arrange
+            var testData = new List<(int heightCm, int weightKg, double expectedResult)>();
+            testData.Add((100, 1, 1.0));
+            testData.Add((200,100,25.0));
+            var target = new BmiCalculator();
+
+            foreach (var pattern in testData)
+            {
+                // Act
+                double actual = target.Calculate(pattern.heightCm, pattern.weightKg);
+                // Assert
+                Assert.AreEqual(pattern.expectedResult, actual,$"height:{pattern.heightCm},weight:{pattern.weightKg},expected:{pattern.expectedResult},actual:{actual}");
+            }
         }
+
+        [TestMethod]
+        public void CalculateExceptionTest()
+        {
+            var target = new BmiCalculator
+            // 0‚ÍŽó‚¯•t‚¯‚È‚¢
+
+        }
+
     }
 }
