a5a9c0dba1fea7257b8dd41261ecb318d842faed
mutsuya masato <mutsuya_masato@yahoo.co.jp>
2020-05-29 16:54:11 +0900

    コンソールアプリケーションからの呼び出しを追加

--- BmiSample/BmiSample.Console/Program.cs
+++ BmiSample/BmiSample.Console/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using BmiSample.Domain;
 
 namespace BmiSample.Console
 {
@@ -6,7 +7,15 @@ namespace BmiSample.Console
     {
         static void Main(string[] args)
         {
-            System.Console.WriteLine("Hello World!");
+            System.Console.WriteLine("身長をセンチメートル単位で入力してください。例：170");
+            string strHeight = System.Console.ReadLine();
+            int intHeight = int.Parse(strHeight);
+            System.Console.WriteLine("体重をキログラム単位で入力してください。例：70");
+            string strWeight = System.Console.ReadLine();
+            int intWeight = int.Parse(strWeight);
+            BmiCalculator calculator = new BmiCalculator();
+            double value = calculator.Calculate(intHeight, intWeight);
+            System.Console.WriteLine($"あなたのBMI指数は {value} です。");
             System.Console.ReadLine();
         }
     }
