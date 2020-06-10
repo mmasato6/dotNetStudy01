05c7fb37fe563337f946c52322aa1ed89b37e397
mutsuya masato <mutsuya_masato@yahoo.co.jp>
2020-06-10 22:48:03 +0900

    最低限の計算機能を実装

--- BmiSample/BmiSample.Wpf/MainWindowViewModel.cs
+++ BmiSample/BmiSample.Wpf/MainWindowViewModel.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Text;
+using BmiSample.Domain;
 
 namespace BmiSample.Wpf
 {
@@ -15,25 +16,50 @@ namespace BmiSample.Wpf
         public int? HeightCm
         {
             get { return _heightCm; }
-            set { _heightCm = value; }
+            set 
+            {
+                _heightCm = value;
+                PropertyChanged(this, new PropertyChangedEventArgs(nameof(HeightCm)));
+                CalculateBmi();
+            }
         }
         private int? _weightKg;
         public int? WeightKg 
         { 
             get { return _weightKg; } 
-            set { _weightKg = value; } 
+            set 
+            { 
+                _weightKg = value;
+                PropertyChanged(this, new PropertyChangedEventArgs(nameof(WeightKg)));
+                CalculateBmi();
+            } 
         }
 
+        private double? _bmiIndex;
         public double? BmiIndex 
         { 
-            get 
-            {
-                return 0.0;
-            }
-        }
+            get {return _bmiIndex; } 
+            private set 
+            { 
+                _bmiIndex = value;
+                PropertyChanged(this, new PropertyChangedEventArgs(nameof(BmiIndex)));
+            } 
+        } 
 
         private void CalculateBmi() 
         {
+            if (!HeightCm.HasValue || HeightCm.Value <= 0) 
+            {
+                BmiIndex = 0.0;
+                return;
+            };
+            if (!WeightKg.HasValue || WeightKg.Value <= 0) 
+            {
+                BmiIndex = 0.0;
+                return;
+            }
+            var calculator = new BmiCalculator();
+            BmiIndex = calculator.Calculate(HeightCm.Value, WeightKg.Value);
         }
     }
 }
