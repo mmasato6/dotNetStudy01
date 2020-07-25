2ec42cd3741c724778ce40b12d6cdbfbeaa47544
mutsuya masato <mutsuya_masato@yahoo.co.jp>
2020-07-25 22:04:00 +0900

    DbContextクラスを作成

---
+++ DiarySample/DiarySample.Dao/DiarySampleDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DiarySample.Dao
+{
+    class DiarySampleDbContext : DbContext
+    {
+        public DiarySampleDbContext(DbContextOptions<DiarySampleDbContext> options) : base(options) { }
+        public DbSet<Diary> Diaries { get; set; }
+    }
+}
