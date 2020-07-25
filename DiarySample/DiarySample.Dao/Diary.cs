31edc3b7536bb09b4bb48e0c57038079aef220bf
mutsuya masato <mutsuya_masato@yahoo.co.jp>
2020-07-25 21:57:46 +0900

    Daoプロジェクトの作成とData Entityの作成

---
+++ DiarySample/DiarySample.Dao/Diary.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DiarySample.Dao
+{
+    public class Diary
+    {
+        public int Id { get; set; }
+        public DateTime RelatedDate { get; set; }
+        public string Title { get; set; }
+        public string Body { get; set; }
+    }
+}
