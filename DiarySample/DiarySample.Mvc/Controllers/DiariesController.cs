6632279c8d86b66ef2b7d6ac9c75e6f4e0f98789
mutsuya masato <mutsuya_masato@yahoo.co.jp>
2022-02-26 16:18:36 +0900

    scaffolding成功

---
+++ DiarySample/DiarySample.Mvc/Controllers/DiariesController.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using DiarySample.Dao;
+
+namespace DiarySample.Mvc.Controllers
+{
+    public class DiariesController : Controller
+    {
+        private readonly DiarySampleDbContext _context;
+
+        public DiariesController(DiarySampleDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Diaries
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Diaries.ToListAsync());
+        }
+
+        // GET: Diaries/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var diary = await _context.Diaries
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (diary == null)
+            {
+                return NotFound();
+            }
+
+            return View(diary);
+        }
+
+        // GET: Diaries/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Diaries/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,RelatedDate,Title,Body")] Diary diary)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(diary);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(diary);
+        }
+
+        // GET: Diaries/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var diary = await _context.Diaries.FindAsync(id);
+            if (diary == null)
+            {
+                return NotFound();
+            }
+            return View(diary);
+        }
+
+        // POST: Diaries/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,RelatedDate,Title,Body")] Diary diary)
+        {
+            if (id != diary.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(diary);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!DiaryExists(diary.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(diary);
+        }
+
+        // GET: Diaries/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var diary = await _context.Diaries
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (diary == null)
+            {
+                return NotFound();
+            }
+
+            return View(diary);
+        }
+
+        // POST: Diaries/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var diary = await _context.Diaries.FindAsync(id);
+            _context.Diaries.Remove(diary);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool DiaryExists(int id)
+        {
+            return _context.Diaries.Any(e => e.Id == id);
+        }
+    }
+}
