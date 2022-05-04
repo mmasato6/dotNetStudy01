beb53e7a07461acc357e583467f2681674d211bf
mutsuya masato <mutsuya_masato@yahoo.co.jp>
2022-05-04 15:17:44 +0900

    ModelにDataAnnotationを付与。 本文を入力する画面の項目をtextareaに変更

--- DiarySample/DiarySample.Dao/Diary.cs
+++ DiarySample/DiarySample.Dao/Diary.cs
@@ -1,12 +1,21 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace DiarySample.Dao
 {
     public class Diary
     {
         public int Id { get; set; }
+        [DataType(DataType.Date)]
+        [Required]
+        [Display(Name ="日付")]
         public DateTime RelatedDate { get; set; }
+        [Required]
+        [StringLength(100)]
+        [Display(Name ="タイトル")]
         public string Title { get; set; }
+        [StringLength(1000)]
+        [Display(Name ="本文")]
         public string Body { get; set; }
     }
 }
